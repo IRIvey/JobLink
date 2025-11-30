edb69432b83d7cfd26aab514f3403b2d011802eb
Zannatul Adon Sabiha <166790186+ZAsabiha@users.noreply.github.com>
2025-11-30 23:08:51 +0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,13 +3,18 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import jobSeekerRoutes from "./routes/jobSeekerRoutes.js"; // NEW
+import jobRoutes from "./routes/jobRoutes.js"; // NEW
+import applicationRoutes from "./routes/applicationRoutes.js"; // NEW
+
+import resumeRoutes from "./routes/resumeRoutes.js"; // ← ADD THIS LINE
 
 dotenv.config();
 
 const app = express();
 
 // Middleware
-app.use(cors()); // Enable CORS for frontend
+app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
@@ -17,10 +22,14 @@ connectDB();
 
 // Routes
 app.get("/", (req, res) => {
-  res.send("JobLink API Running with Authentication");
+  res.send("JobLink API Running");
 });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/resume", resumeRoutes); // ← ADD THIS LINE
+app.use("/api/jobseeker", jobSeekerRoutes); // NEW
+app.use("/api/jobs", jobRoutes); // NEW
+app.use("/api/applications", applicationRoutes); // NEW
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
