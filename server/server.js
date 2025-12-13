0992c630f5795b09d49089fd1fded4a6497cecc3
Zannatul Adon Sabiha <166790186+ZAsabiha@users.noreply.github.com>
2025-12-13 13:08:38 +0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -32,5 +32,6 @@ app.use("/api/jobs", jobRoutes);
 app.use("/api/applications", applicationRoutes);
 app.use("/api/companies", companyRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on ${PORT}`));
+
